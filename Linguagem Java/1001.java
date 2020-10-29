ea19c31fbd830e1ba076f0e50dca4e2c3a1b7314
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-10-29 09:45:11 -0300

    foi criada a pasta java com o exercicio 1 da URI

---
+++ Linguagem Java/1001.java
@@ -0,0 +1,19 @@
+package pkg1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, x;
+        
+        a = input.nextInt();
+        b = input.nextInt();
+        x = a+b;
+        
+        System.out.println("X = "+x);
+        
+        
+    }
+    
+}
