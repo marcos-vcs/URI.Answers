1ff39c6f2f82539330ad5ff6111deb837834c2f2
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-10-29 10:50:33 -0300

    resposta exercicio 1004 URI

---
+++ Linguagem Java/1004.java
@@ -0,0 +1,19 @@
+
+package pkg1004;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int PROD = a*b;
+            System.out.println("PROD = "+PROD);
+        
+        
+        
+    }
+    
+}
