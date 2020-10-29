45c6ea8823e3ed03be1c939c9461ddbb3d7276e6
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-10-29 10:32:01 -0300

    adicionei o 1002

---
+++ Linguagem Java/1002.java
@@ -0,0 +1,20 @@
+package pkg1002;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+		double pi = 3.14159;
+		double raio = input.nextDouble();
+		double area = pi * Math.pow(raio, 2);
+		System.out.printf("A=%.4f", area);
+		System.out.println();
+            
+            
+            
+               
+    }
+    
+}
