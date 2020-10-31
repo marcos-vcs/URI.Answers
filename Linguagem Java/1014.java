b63831c7c72fe3676b6be0e4fb8e8b6a8be1a61d
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-10-31 19:43:38 -0300

    Create 1014

---
+++ Linguagem Java/1014.java
@@ -0,0 +1,18 @@
+package pkg1014;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+        
+        x = x/y;
+        
+        System.out.printf("%.3f km/l\n", x);
+        
+    }
+    
+}
