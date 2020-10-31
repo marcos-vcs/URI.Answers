2fe8c96d9c250810c643a30190feed56792b1abf
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-10-31 20:36:44 -0300

    Create 1016

---
+++ Linguagem Java/1016.java
@@ -0,0 +1,18 @@
+package pkg1016;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int distanciaKM = input.nextInt();
+        
+        distanciaKM = distanciaKM * 2;
+        
+        System.out.println(distanciaKM+" minutos");
+        
+        
+    }
+    
+}
