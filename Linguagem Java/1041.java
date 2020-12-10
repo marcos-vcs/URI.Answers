a1d20445db85fb532386ad0223643f9e5086fc48
Marcos Sousa <37250628+marcosviniciocardososousa@users.noreply.github.com>
2020-12-10 12:51:28 -0300

    Create 1041

---
+++ Linguagem Java/1041.java
@@ -0,0 +1,30 @@
+package pkg1041;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        float x = input.nextFloat();
+        float y = input.nextFloat();
+        
+     if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else if (x == 0) {
+            System.out.println("Eixo Y");
+        } else if (y == 0) {
+            System.out.println("Eixo X");
+        } else if (y > 0 && x > 0) {
+            System.out.println("Q1");
+        } else if (y > 0 && x < 0) {
+            System.out.println("Q2");
+        } else if (y < 0 && x < 0) {
+            System.out.println("Q3");
+        } else if (y < 0 && x > 0) {
+            System.out.println("Q4");
+        }
+    }
+    
+}
+
